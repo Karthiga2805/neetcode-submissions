132c1362253b60b4b0e5cda1b8aa3543fdee5d01
Karthiga2805 <140970363+Karthiga2805@users.noreply.github.com>
2026-07-07 23:46:40 +0530

    Add: two-integer-sum - submission-7

---
+++ Data Structures & Algorithms/two-integer-sum/submission-7.py
@@ -0,0 +1,17 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Set = set()  # Even though this looks efficient(avoids the recalcualtion and looping for numbers already calculated), it takes more time than the one where you dont use set
+        # for i in range(len(nums)):
+        #     if nums[i] not in Set:
+        #         Set.add(nums[i])
+        #         if ((target - nums[i]) in nums[i+1:]):
+        #             nums_dup  = nums[i+1:]
+        #             index = nums_dup.index(target-nums[i]) + i + 1
+        #             return [i,index]          
+        # return []
+        for i in range(len(nums)):
+            if ((target - nums[i]) in nums[i+1:]):
+                nums_dup  = nums[i+1:]
+                index = nums_dup.index(target-nums[i]) + i + 1
+                return [i,index]          
+        return []
