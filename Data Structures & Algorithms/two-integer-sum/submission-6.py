f9e7dbf42abb0e6bd132b0c7c507d6d8937d679b
Karthiga2805 <140970363+Karthiga2805@users.noreply.github.com>
2026-07-07 23:44:18 +0530

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.py
@@ -0,0 +1,12 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        Set = set()
+        for i in range(len(nums)):
+            if nums[i] not in Set:
+                Set.add(nums[i])
+                if ((target - nums[i]) in nums[i+1:]):
+                    nums_dup  = nums[i+1:]
+                    index = nums_dup.index(target-nums[i]) + i + 1
+                    return [i,index]          
+        return []
+
